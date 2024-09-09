c2f9e792db3d6db9fc98d852c633a7b3a38d9bbe
Shashank Bhushan <sbsb4961@gmail.com>
2024-09-09 00:21:54 -0500

    committed on 9th September 2024

---
+++ Python_Basics/lists2.py
@@ -0,0 +1,20 @@
+# Exercise List Methods
+# SCROLL FOR ANSWERS!
+# using this list,
+basket = ["Banana", "Apples", "Oranges", "Blueberries"]
+
+# 1. Remove the Banana from the list
+print(basket.pop(0))
+# 2. Remove "Blueberries" from the list.
+print(basket.pop(2))
+# 3. Put "Kiwi" at the end of the list.
+basket.append("Kiwi")
+print(basket)
+# 4. Add "Apples" at the beginning of the list
+basket.insert(0,"Apples")
+print(basket)
+# 5. Count how many apples in the basket
+print(basket.count("Apples"))
+# 6. empty the basket
+basket.clear()
+print(basket)
