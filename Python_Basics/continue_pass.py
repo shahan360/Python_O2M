74d2ecd02568c1806bcc4f6a4a50765ce17bbd4c
Shashank Bhushan <sbsb4961@gmail.com>
2024-09-12 11:21:22 -0500

    committed on 12th September 2024

---
+++ Python_Basics/continue_pass.py
@@ -0,0 +1,8 @@
+# 'continue' will skip the lines below it and continue to loop.
+for i in range(0,5):
+    print(i)
+    continue # will continue the next iteration
+    print("This will not print")
+
+for i in range(10):
+    pass # it can be used to avoid error and when we have not yet decided on the code to write.
