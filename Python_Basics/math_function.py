79990a82a27c92de7bf752df88d75e7fa311cc5d
Shashank Bhushan <sbsb4961@gmail.com>
2024-09-11 20:42:27 -0500

    committed on 11th September 2024

---
+++ Python_Basics/math_function.py
@@ -0,0 +1,5 @@
+print(round(3.1))
+print(round(3.5))
+print(abs(0))
+print(abs(-3.1))
+print(abs(3.1))
---
+++ Python_Basics/multidimensional_lists.py
@@ -0,0 +1,3 @@
+# these type of lists are also known as Matrix
+basket = ["Banana",["Apples",["Oranges"],"Blueberries"]]
+print(basket[1][1][0]) # returns Oranges
