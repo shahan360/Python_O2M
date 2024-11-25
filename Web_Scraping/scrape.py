20d9946832ff211c44050568008c9a669ce601e3
shahan360 <sbsb4961@gmail.com>
2024-11-25 16:20:05 -0600

    committed on 25th Nov 2024

---
+++ Web_Scraping/scrape.py
@@ -0,0 +1,4 @@
+# BeautifulSoup 4 documentation: https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+# Information about selectors: https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Selectors
+
