70dfc85e5db1f5bb9dc9a005a1b2699a8bb06420
shahan360 <sbsb4961@gmail.com>
2024-11-28 09:29:05 -0600

    committed on 28th Nov 2024

---
+++ Web_Development/info.py
@@ -0,0 +1,7 @@
+# link to flask documentation: https://flask.palletsprojects.com/en/1.1.x/quickstart/
+# link to django documentation: https://docs.djangoproject.com/en/3.0/intro/tutorial01/
+
+# Flask and Django are both web frameworks for Python. 
+# They are used to build web applications.
+# Flask is a microframework, meaning it is lightweight and flexible.
+# Django is a high-level framework, meaning it is more complex and feature-rich.
