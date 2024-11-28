e1406b3e01f005768ee98151b09b92c1cfbc5e5f
shahan360 <sbsb4961@gmail.com>
2024-11-28 07:24:20 -0600

    committed on 28th Nov 2024

---
+++ ML_DS/ml.py
@@ -0,0 +1,59 @@
+#Facebook Machine Learning Video Series: https://research.facebook.com/blog/2018/05/the-facebook-field-guide-to-machine-learning-video-series/
+
+#ML Steps:
+#1. Data Collection
+#2. Data Preprocessing
+#3. Feature Engineering
+#4. Model Training
+#5. Model Evaluation
+#6. Model Deployment
+#7. Model Monitoring
+#8. Model Maintenance
+#9. Model Update
+#10. Model Retrain
+#11. Model Refinement
+
+# Pandas Cheat Sheet Notebook: https://github.com/practicalAI/practicalAI/blob/4ad626098aca25db5628fe67895e738d5a5c2c2a/notebooks/03_Pandas.ipynb
+
+# Kaggle: https://www.kaggle.com/
+
+# Python Cheat Sheet for Data Science: https://elitedatascience.com/python-cheat-sheet
+
+# Bokeh library documentation: https://docs.bokeh.org/en/latest/index.html
+# Bokeh library tutorial: https://docs.bokeh.org/en/latest/docs/user_guide/quickstart.html
+# Link to the Seaborn library: https://seaborn.pydata.org/
+# Link to the Matplotlib library: https://matplotlib.org/
+# Link to the Scikit-learn library: https://scikit-learn.org/stable/
+# Link to the Pandas library: https://pandas.pydata.org/
+# Link to the Numpy library: https://numpy.org/
+# Link to the IRIS dataset: https://archive.ics.uci.edu/ml/datasets/iris
+# Information on the IRIS dataset: https://en.wikipedia.org/wiki/Iris_flower_data_set
+
+# Machine Learning Steps Simplified:
+#1. Import the data
+#2. Clean the data
+#3. Split the data into training/test sets
+#4. Train the model
+#5. Test the model to check the output
+#6. Evaluate the model to improve accuracy
+
+# Columns in the IRIS dataset:
+#1. Sepal length in cm
+#2. Sepal width in cm
+#3. Petal length in cm
+#4. Petal width in cm
+#5. Class: Iris Setosa, Iris Versicolour, Iris Virginica
+
+# link to the Nearest Neighbors algorithm: https://scikit-learn.org/stable/modules/neighbors.html
+
+# learn more about K-Nearest Neighbours algorithm for classification, with an interactive demo to understand the parameters use, including the N value: http://vision.stanford.edu/teaching/cs231n-demos/knn/
+
+# how KNN algorithm works: https://www.analyticsvidhya.com/blog/2018/08/k-nearest-neighbor-introduction-regression-python/
+
+# What is Joblib? 
+# Joblib is a set of tools to provide lightweight pipelining in Python. 
+# In particular, joblib offers: transparent disk-caching of the output values and lazy re-evaluation (memoize pattern). 
+# This is especially useful when your function is I/O bound, such as reading data from disk, downloading data from the internet, or querying a database.
+
+# How to import the joblib library: 
+# from joblib import dump, load
