ffd45a0c57f0127421abcca449fbc10c08d6832a
shahan360 <sbsb4961@gmail.com>
2024-11-28 09:57:16 -0600

    committed on 28th Nov 2024

--- Web_Development/info.py
+++ Web_Development/info.py
@@ -5,3 +5,18 @@
 # They are used to build web applications.
 # Flask is a microframework, meaning it is lightweight and flexible.
 # Django is a high-level framework, meaning it is more complex and feature-rich.
+
+# to create a virtual environment with name 'venv' in Mac ARM64 bash use below command in the shell terminal opened in the project directory:
+# python3 -m venv venv
+
+# to activate the virtual environment in Mac ARM64 bash use below command in the shell terminal opened in the project directory:
+# source venv/bin/activate
+
+# to install flask in the virtual environment use below command in the shell terminal opened in the project directory:
+# pip3 install Flask
+
+# documentation on venv: https://docs.python.org/3/library/venv.html
+
+# refresher on virtual environments in Python: https://realpython.com/python-virtual-environments-a-primer/
+
+# where our files should be in relation to the virtual environment: https://stackoverflow.com/questions/1783146/where-in-a-virtualenv-does-the-custom-code-go
