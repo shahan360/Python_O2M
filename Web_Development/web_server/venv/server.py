d9826d132c1c11fa78f1d82e2e2f5678c9748129
shahan360 <sbsb4961@gmail.com>
2024-11-29 23:40:32 -0600

    committed on 29th Nov 2024

--- Web_Development/web_server/venv/server.py
+++ Web_Development/web_server/venv/server.py
@@ -5,3 +5,27 @@ print(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+#quickstart guide to flask app: https://flask.palletsprojects.com/en/stable/quickstart/
+
+#flask templates: http://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
+
+#flask static files: http://flask.palletsprojects.com/en/1.1.x/quickstart/#static-files
+
+#flask favicon: http://flask.palletsprojects.com/en/1.1.x/patterns/favicon/
+
+#flask jinja2: https://en.wikipedia.org/wiki/Jinja_(template_engine)
+#flask jinja2: https://jinja.palletsprojects.com/en/2.11.x/
+
+#create dynamic URLs: https://stackoverflow.com/questions/7478366/create-dynamic-urls-in-flask-with-url-for
+#variable rules: https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
+
+
+
+@app.route('/thoughts')
+def thoughts():
+    return 'It starts from 0ne and taken to 1ne!'
+
+@app.route('/thoughts2')
+def thoughts2():
+    return 'It starts from 0ne and taken to 1ne!'
